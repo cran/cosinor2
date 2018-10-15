d036dd0244854394a24d9b3148a16a6bcb835e5a
Augustin Mutak <mutak94@gmail.com>
2018-10-15 15:10:03 +0000

    version 0.2.1

--- inst/doc/cosinor2.R
+++ inst/doc/cosinor2.R
@@ -4,10 +4,10 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- echo = F-----------------------------------------------------------
+## ---- echo = F, message = F, warning = F---------------------------------
 library(cosinor2)
 
-## ------------------------------------------------------------------------
+## ---- fig.align = "center", fig.height = 6, fig.width = 6----------------
 fit.panas.cosinor <- population.cosinor.lm(data = PANAS_november, time = PANAS_time, period = 7)
 
 ## ------------------------------------------------------------------------
@@ -15,7 +15,9 @@ fit.panas.cosinor$conf.ints
 
 ## ------------------------------------------------------------------------
 fit.panas.cosinor$emp.mean
+
 fit.panas.cosinor$fitted.values
+
 fit.panas.cosinor$residuals
 
 ## ------------------------------------------------------------------------
@@ -45,20 +47,20 @@ cosinor.PR(fit.panas.cosinor)
 ## ------------------------------------------------------------------------
 cosinor.PR(fit.temp.cosinor)
 
-## ------------------------------------------------------------------------
+## ---- message = F, warning = F, results = "hide", fig.align = "center", fig.width = 6, fig.height = 6----
 periodogram(data = PANAS_november, time = PANAS_time)
 
-## ------------------------------------------------------------------------
+## ---- message = F, warning = F, results = "hide", fig.align = "center", fig.width = 6, fig.height = 6----
 periodogram(data = PA_extraverts, time = PA_time, periods = 1:24)
 
-## ------------------------------------------------------------------------
+## ---- fig.align = "center", fig.height = 6, fig.width = 6----------------
 fit.pa_ext.cosinor <- population.cosinor.lm(data = PA_extraverts, time = PA_time, period = 24)
 fit.pa_int.cosinor <- population.cosinor.lm(data = PA_introverts, time = PA_time, period = 24)
 
 ## ------------------------------------------------------------------------
 cosinor.poptests(fit.pa_ext.cosinor, fit.pa_int.cosinor)
 
-## ------------------------------------------------------------------------
+## ---- message = F, warning = F, results = "hide", fig.width = 6, fig.height = 12----
 fit.panas.ssections <- ssections(data = PANAS_november, time = PANAS_time, period = 7, interval = 7, increment = 1)
 
 ## ------------------------------------------------------------------------
@@ -66,6 +68,7 @@ fit.panas.ssections$emp.mean
 
 ## ------------------------------------------------------------------------
 fit.panas.ssections$coefficients
+
 fit.panas.ssections$`p-values`
 
 ## ------------------------------------------------------------------------
